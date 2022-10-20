f9c4bf2739916d0316e27c920d37c56c9f0f5ee5
Айяр Халилов <super.ayar-04@yandex.ru>
2022-10-20 21:29:39 +0300

    Code maintenance

--- static/index.js
+++ static/index.js
@@ -1,29 +1,29 @@
 function calcWithAddons (initialPrice) {
-    let cost = initialPrice;
-    let e_amount = document.querySelector("#calculator__amount");
+    let cost = initialPrice
+    let e_amount = document.querySelector("#calculator__amount")
 
-    const parsedAmount = parseFloat(e_amount.value);
-    let amount = Math.max((Number.isInteger(parsedAmount) && parsedAmount) || 0, 0);
-    const e_checkedInputs = document.querySelectorAll("#calculatorForm input:checked");
+    const parsedAmount = parseFloat(e_amount.value)
+    let amount = Math.max((Number.isInteger(parsedAmount) && parsedAmount) || 0, 0)
+    const e_checkedInputs = document.querySelectorAll("#calculatorForm input:checked")
     e_checkedInputs.forEach((e_checkedInput) => {
-        cost += parseInt(e_checkedInput.dataset.price);
-    });
-    return cost * amount;
+        cost += parseInt(e_checkedInput.dataset.price)
+    })
+    return cost * amount
 }
 
 function renderFinalCost(meal) {
-    const e_finalCost = document.querySelector("#calculator__finalCost");
-    e_finalCost.innerHTML = calcWithAddons(meal.price);
+    const e_finalCost = document.querySelector("#calculator__finalCost")
+    e_finalCost.innerHTML = calcWithAddons(meal.price)
 }
 
 function renderRadios(radios) {
-    let str = "";
+    let str = ""
 
     if (radios) {
         for (const [radioGroupName, radioGroup] of Object.entries(radios)) {
-            str += `<label class="form-label">${radioGroup.description}</label>`;
+            str += `<label class="form-label">${radioGroup.description}</label>`
             for (const [radioName, radio] of Object.entries(radioGroup.choices)) {
-                radioInputId = `calculator__${radioGroupName}_${radioName}`;
+                radioInputId = `calculator__${radioGroupName}_${radioName}`
                 str += `
                     <div class="form-check">
                         <input class="form-check-input"
@@ -39,18 +39,18 @@ function renderRadios(radios) {
                             ${radio.description} - ${radio.price} руб.
                         </label>
                     </div>
-                `;
+                `
             }
         }
     }
 
-    return str;
+    return str
 }
 
 function renderCheckboxes(checkboxes) {
-    let str = "";
+    let str = ""
     for (const [checkboxName, checkbox] of Object.entries(checkboxes)) {
-        const checkboxInputId = `calculator__${checkboxName}`;
+        const checkboxInputId = `calculator__${checkboxName}`
         str += `
             <div class="form-check">
                 <input class="form-check-input"
@@ -64,81 +64,81 @@ function renderCheckboxes(checkboxes) {
                     ${checkbox.description} - ${checkbox.price} руб.
                 </label>
             </div>
-        `;
+        `
     }
-    return str;
+    return str
 }
 
 function renderCheckboxBlock(checkboxes) {
-    let str = "";
+    let str = ""
     if (checkboxes) {
         str += `
             <div class="mb-3">
                 <label class="form-label">Добавить</label>
                 ${renderCheckboxes(checkboxes)}
             </div>
-        `;
+        `
     }
-    return str;
+    return str
 }
 
 function renderForm(meal) {
     return new Promise((resolve, reject) => {
-        document.querySelector("#calculator__amount").value = 1;
-        document.querySelector("#calculator__radios").innerHTML = renderRadios(meal.radios);
-        document.querySelector("#calculator__checkboxes").innerHTML = renderCheckboxBlock(meal.checkboxes);
-        renderFinalCost(meal);
+        document.querySelector("#calculator__amount").value = 1
+        document.querySelector("#calculator__radios").innerHTML = renderRadios(meal.radios)
+        document.querySelector("#calculator__checkboxes").innerHTML = renderCheckboxBlock(meal.checkboxes)
+        renderFinalCost(meal)
 
-        const onInputsUpdate = () => renderFinalCost(meal);
+        const onInputsUpdate = () => renderFinalCost(meal)
 
         document.querySelectorAll("#calculator__radios input, #calculator__checkboxes input").forEach((input) => {
-            input.addEventListener("change", onInputsUpdate);
-        });
-        document.querySelector("#calculator__amount").addEventListener("input", onInputsUpdate);
+            input.addEventListener("change", onInputsUpdate)
+        })
+        document.querySelector("#calculator__amount").addEventListener("input", onInputsUpdate)
 
-        resolve();
-    });
+        resolve()
+    })
 }
 
 function loadImage(src) {
     return new Promise((resolve, reject) => {
-        const image = new Image();
-        image.src = src;
-        image.addEventListener('load', () => resolve(image.src));
-        image.addEventListener('error', reject);
-    });
+        const image = new Image()
+        image.src = src
+        image.addEventListener('load', () => resolve(image.src))
+        image.addEventListener('error', reject)
+    })
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    const e_mealTypeSelector = document.querySelector("#calculator__meal");
-    const e_bgCanvas = document.querySelector("#bg-canvas");
+    const e_mealTypeSelector = document.querySelector("#calculator__meal")
+    const e_bgCanvas = document.querySelector("#bg-canvas")
 
-    const menu = await fetch("/menu.json").then(res => res.json());
+    const menu = await fetch("/menu.json").then(res => res.json())
 
     for (const [mealType, meal] of Object.entries(menu)) {
         e_mealTypeSelector.innerHTML += `<option value="${mealType}">${meal.description} - ${meal.price} руб.</option>`
     }
 
     renderForm(menu[e_mealTypeSelector.value]).then(() => {
-        const card = document.querySelector("#card");
-        card.classList.add("opacity-100");
-    });
+        const card = document.querySelector("#card")
+        card.classList.add("opacity-100")
+    })
 
     setTimeout(() => {
-        document.querySelector("#card").classList.add("vanishIn");
-    }, 1000);
+        document.querySelector("#card").classList.add("vanishIn")
+    }, 1000)
 
 
     loadImage("https://www.grandactive.ru/UploadedFiles/2021/2021-02/c809089d-ca27-41a0-8a5f-d592a60cedbe.jpg").then(function(src) {
-       e_bgCanvas.style.backgroundImage = `url(${src})`;
+       e_bgCanvas.style.backgroundImage = `url(${src})`
        setTimeout(function() {
-           e_bgCanvas.classList.add("opacity-100");
+           e_bgCanvas.classList.add("opacity-100")
        }, 500)
-    });
+    })
 
     e_mealTypeSelector.addEventListener("change", () => {
-        renderForm(menu[e_mealTypeSelector.value]);
-    });
+        renderForm(menu[e_mealTypeSelector.value])
+    })
 
-});
+})
